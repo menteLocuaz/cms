254dd2cbe5fc1cbb1b07c1311007ab0fcf2c435e
menteLocuaz <emersondurate.45@gmail.com>
2026-07-01 12:55:04 -0500

    feat:composer

--- index.php
+++ index.php
@@ -1,28 +1,31 @@
-<?php 
+<?php
 
 /*=============================================
-Depurar Errores
-=============================================*/
+ * Depurar Errores
+ * =============================================*/
 
-define('DIR',__DIR__);
+define('DIR', __DIR__);
 
-ini_set("display_errors", 1);
-ini_set("log_errors", 1);
-ini_set("error_log", DIR."/php_error_log");
-
-/*=============================================
-Requerimientos
-=============================================*/
-
-require_once "controllers/template.controller.php";
-require_once "controllers/curl.controller.php";
-require_once "extensions/vendor/autoload.php";
+ini_set('display_errors', 1);
+ini_set('log_errors', 1);
+ini_set('error_log', DIR . '/php_error_log');
 
 /*=============================================
-Plantilla
-=============================================*/
+ * Requerimientos
+ * =============================================*/
+
+require_once DIR . '/vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->safeLoad();
+
+require_once 'controllers/template.controller.php';
+require_once 'controllers/curl.controller.php';
+
+/*=============================================
+ * Plantilla
+ * =============================================*/
 
 $index = new TemplateController();
-$index -> index();
+$index->index();
 
-?>
